6586c9ed274424ca88b47119d2d9ad740a5a711e
unknown <andrew1994android@gmail.com>
2021-07-07 11:37:25 +0100

    Adding Router Tunnel to the script

--- SetupVPN.ps1
+++ SetupVPN.ps1
@@ -1,15 +1,18 @@
-$Status = Get-VpnConnection -Name $vpnname 
 function VPNCreation {
     Add-VpnConnection -Name (Read-Host -Prompt "Enter Company Name") -ServerAddress (Read-Host -Prompt "Enter Router or domain address") -TunnelType "l2tp" -L2tpPsk (Read-Host -Prompt "Enter L2TP Password") -EncryptionLevel "Required" -AuthenticationMethod MSChapv2 -SplitTunneling -PassThru
 }
 VPNCreation
 
+$VPNConnection = Get-VpnConnection | Select-Object
+function VPNTunnel {
+    Add-VPNConnectionRoute -ConnectionName $VPNConnection.Name -DestinationPrefix (Read-Host -Prompt "Enter Subnet Prefix i.e 10.10.0.0/24")
+}
+
 function ConnectVPN {
-$vpnName = Read-Host -Prompt "Enter VPN Name"
+$vpnName = $VPNConnection.Name
 $vpn = Get-VpnConnection -Name $vpnName;
 if($vpn.ConnectionStatus -eq "Disconnected"){
 rasdial $vpnName (Read-Host -Prompt "Enter VPN Username") (Read-Host -Prompt "Enter Password")
 }
 }
 ConnectVPN
-
